2a6c2ec6678677e179fb1064100675b0007e1250
Africanaerial <166567291+Africanaerial@users.noreply.github.com>
2024-04-09 17:55:35 -0600

    Update tromingeode.js

--- tromingeode.js
+++ tromingeode.js
@@ -1,3 +1,5 @@
 function createGame() {
-  
+  var welcomePage = prompt("Welcome to Tromingeode!");
+  var userName = prompt("Please select a name");
+  alert("Welcome" + " " + userName + " " + "to Tromingeode.");
 }
