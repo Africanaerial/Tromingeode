d86c4ce2bce1be89df50954c73203c9d1c3d2edc
Africanaerial <166567291+Africanaerial@users.noreply.github.com>
2024-04-10 16:42:19 -0600

    Update tromingeode.js

--- tromingeode.js
+++ tromingeode.js
@@ -6,6 +6,9 @@ function createGame() {
     var chosenClass = prompt("Please select a class. (Archeroid)");
     var Archeroid;
     function createClassOne() {
+  
+  }
+    if(chosenClass = Archeroid) {
   alert("You have chosen:" + " " + "chosenClass");
   alert("Archeroids start with 5 main abilities. They are: Moonlight Meteor, Solar Sear, Scorched Ice, Xenon Blast, and Area Reborn.");
   alert("Moonlight Meteor: Spell type: AoE. Starting Power: 50 DPS. Maximum Time (current): 5 secs. Description: A random meteor preserved in the Doom Moon's core for many millenniums.");
@@ -14,9 +17,6 @@ function createGame() {
   alert("Xenon Blast: Spell type: Projectile. Starting Power: 192 DPS. Maximum Time (current): 3 secs. Description: This spell does not last that long, but its power makes up for it.");
   alert("Area Reborn: Spell type: Ultimate. Starting Power: 395 DPS. Maximum Time (current): 11 secs. Description: They gather anything powerful they could find in the Nine Platforms Of Life and Death.");
   alert("You will be given these starter abilities.");
-  }
-    if(chosenClass = Archeroid) {
-  createClassOne();
   }
  }
 characterClasses();
