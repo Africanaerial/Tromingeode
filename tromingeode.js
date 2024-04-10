6c87928cc72960d670cf6bec67a9171ee430ab16
Africanaerial <166567291+Africanaerial@users.noreply.github.com>
2024-04-09 22:55:18 -0600

    Update tromingeode.js

--- tromingeode.js
+++ tromingeode.js
@@ -2,5 +2,23 @@ function createGame() {
   var welcomePage = prompt("Welcome to Tromingeode!");
   var userName = prompt("Please select a name");
   alert("Welcome" + " " + userName + " " + "to Tromingeode.");
-  var classOne = Acrheroid;
+  function characterClasses() {
+    var chosenClass = prompt("Please select a class.");
+    var classOne = Archeroid;
+    if(chosenClass = Archeroid) {
+    function createClassOne() {
+  alert("You have chosen:" + " " + "chosenClass");
+  alert("Archeroids start with 5 main abilities. They are: Moonlight Meteor, Solar Sear, Scorched Ice, Xenon Blast, and Area Reborn.");
+  alert("Moonlight Meteor: Spell type: AoE. Starting Power: 50 DPS. Maximum Time (current): 5 secs. Description: A random meteor preserved in the Doom Moon's core for many millenniums.");
+  alert("Solar Sear: Spell type: Projectile. Starting Power: 79 DPS. Maximum Time (current): 7 secs. Description: This spell was created by a group of Archeroids, who created a power stone made from the flares and dust particles of the Death Sun.");
+  alert("Scorched Ice: Spell type: Defense. Starting Power: 30 DPS + Protection. Maximum Time (current): 10 secs. Description: Rised from permafrost but did not become permafrost. It has been considered failed permafrost but maybe it has a second chance to reforge in The Ocean Of Permafrost.");
+  alert("Xenon Blast: Spell type: Projectile. Starting Power: 192 DPS. Maximum Time (current): 3 secs. Description: This spell does not last that long, but its power makes up for it.");
+  alert("Area Reborn: Spell type: Ultimate. Starting Power: 395 DPS. Maximum Time (current): 11 secs. Description: They gather anything powerful they could find in the Nine Platforms Of Life and Death.");
+  alert("You will be given these starter abilities.");
+  }
+  createClassOne();
+  }
+ }
+characterClasses();
 }
+createGame();
