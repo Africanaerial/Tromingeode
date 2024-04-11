c71e6e9d482da2499bb360d88c07e6fcab0586d2
Africanaerial <166567291+Africanaerial@users.noreply.github.com>
2024-04-10 19:34:16 -0600

    Update tromingeode.js

--- tromingeode.js
+++ tromingeode.js
@@ -2,9 +2,11 @@ function createGame() {
   var welcomePage = prompt("Welcome to Tromingeode!");
   var userName = prompt("Please select a name");
   alert("Welcome" + " " + userName + " " + "to Tromingeode.");
-  function characterClasses() {
-    var Archeroid;
-}
-characterClasses();
+  var Archeroid;
+  var classchosen = prompt("Select a class. Archeroid.");
+  if(classchosen = "Archeroid") {
+    alert("You have chosen Archeroid.");
+    alert("Archeroid's main abilities are Moonlight Meteor, Solar Sear, Scorched Ice, Xenon Blast, and Area Reborn.");
+    }
 }
 createGame();
