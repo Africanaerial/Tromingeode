bfbdcdecdc654570c0d3eb48c6a39d3d52986d26
Africanaerial <166567291+Africanaerial@users.noreply.github.com>
2024-04-10 22:58:20 -0600

    Update tromingeode.js

--- tromingeode.js
+++ tromingeode.js
@@ -6,12 +6,13 @@ function Game() {
         alert("Your name has been saved to the game code.");
         }
         else if(confirmUsername = "no") {
-            for(let i = 0; i < confirmUsername; i++) {
+            while(confirmUsername = "no") {
+                username;
+                confirmUsername;
                 if(confirmUsername = "yes") {
-                    break;
                     alert("Your name has been saved to the game code.");
                     }
-                }
+            }
             }
 }
 Game();
