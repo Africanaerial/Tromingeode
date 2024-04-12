84e072e53b3c12cf5bccb9df01f002804d2b705d
Africanaerial <166567291+Africanaerial@users.noreply.github.com>
2024-04-11 21:23:55 -0600

    Update tromingeode.js

--- tromingeode.js
+++ tromingeode.js
@@ -1,17 +1,27 @@
 function Game() {
     alert("Welcome to Tromingeode.");
-    var username = prompt("Please enter a username. DO NOT USE YOUR REAL NAME.");
+    var userName = prompt("Please enter a username. DO NOT USE YOUR REAL NAME.");
     var confirmUsername = prompt("You have chosen" + " " + username + " " + "as your name. Do you confirm? yes/no");
     if(confirmUsername = "yes") {
         alert("Your name has been saved to the game code.");
         }
         if(confirmUsername = "no") {
             while(confirmUsername = "no") {
-                username;
+                userName;
                 confirmUsername;
                 if(confirmUsername = "yes") {
-                    break;
+                    return userName;
+                    const userName = userName;
                     }
+                    else() {
+                        for(let i = 0; i < confirmUsername; i++) {
+                            userName;
+                            confirmUsername;
+                            if(confirmUsername = "yes") {
+                                return userName;
+                                const userName = userName;
+                            }
+                        }
             }
             }
 }
